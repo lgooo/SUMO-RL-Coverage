b456334c928d069a58ea6cac91a016eb6414139e
Kihyuk Hong <kihyukh@umich.edu>
2022-06-15 22:57:54 -0400

    yaml

--- sumo_gym/run.py
+++ sumo_gym/run.py
@@ -4,6 +4,7 @@
 import argparse
 from sumo_gym import SumoGym
 import numpy as np
+import yaml
 
 Observation = np.ndarray
 Action = np.ndarray
@@ -12,8 +13,8 @@ parser = argparse.ArgumentParser(
     description='SUMO Gym Tester')
 parser.add_argument(
     '--config',
-    default='sumo_configs/simple.sumocfg',
-    help='SUMO config file path')
+    default='config/simple.yaml',
+    help='config file path')
 parser.add_argument(
     '--delta_t',
     type=float,
@@ -21,7 +22,14 @@ parser.add_argument(
     help='simulation time step')
 args = parser.parse_args()
 
-env = SumoGym(sumo_config=args.config, delta_t=args.delta_t, render_flag=True)
+with open(args.config, 'r') as f:
+    conf = yaml.safe_load(f)
+
+env = SumoGym(
+    sumo_config=conf['env']['sumo_config'],
+    delta_t=args.delta_t,
+    render_flag=True,
+)
 
 obs = env.reset()
 def policy(obs: Observation) -> Action:
