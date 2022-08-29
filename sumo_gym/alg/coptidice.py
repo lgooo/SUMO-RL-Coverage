8f17983e8e217bc1978fc47543401c212739be1a
Kihyuk Hong <kihyukh@umich.edu>
2022-08-29 16:07:34 -0400

    loss function for COptiDICE

--- sumo_gym/alg/coptidice.py
+++ sumo_gym/alg/coptidice.py
@@ -35,20 +35,115 @@ class COptiDICE(Alg):
         self.config = config
         self.n_actions = n_actions
 
+        self.gamma = config.get('gamma', 0.95)
+        self.alpha = config.get('alpha', 1)
         self.memory_size = config.get('memory_size', 1e5)
+        self.f_type = config.get('f_type', 'kl')
+        self.num_costs = config.get('num_costs', 3) # TODO: this is hacky
+        self.c_hats = torch.ones(self.num_costs) * config.get('cost_thresholds', 1)
+        self.cost_ub_eps = config.get('cost_ub_epsilon', 0)
 
         self.nu_network = MLP(n_states, 1, [128, 128])
         self.chi_network = MLP(n_states, 1, [128, 128])
         self.policy_network = MLP(n_states, n_actions, [128, 128], 'softmax')
 
+        self.lamb = torch.zeros(self.num_costs, requires_grad=True)
+        self.tau = torch.zeros(self.num_costs, requires_grad=True)
+
         self.memory = Deque(capacity=int(self.memory_size), multi_steps=1)
 
+    def f(self, x):
+        if self.f_type == 'chisquare':
+            return 0.5 * ((x - 1) ** 2)
+        if self.f_type == 'softchi':
+            return np.where(
+                x < 1,
+                x * (np.log(x + 1e-10) - 1) + 1,
+                0.5 * ((x - 1) ** 2)
+            )
+        if self.f_type == 'kl':
+            x * np.log(x + 1e-10)
+
+        raise NotImplementedError('undefined f_type', self.f_type)
+
+    def f_prime_inv(self, x):
+        if self.f_type == 'chisquare':
+            return x + 1
+        if self.f_type == 'softchi':
+            return np.where(
+                x < 0,
+                np.exp(np.minimum(x, 0)),
+                x + 1
+            )
+        if self.f_type == 'kl':
+            return np.exp(x - 1)
+
+        raise NotImplementedError('undefined f_type', self.f_type)
+
     def update(self):
-        # nu loss
-        """
-        nu_loss = (1 - gamma) * jnp.mean(f_init['nu'])
-        nu_loss += -alpha * jnp.mean(f_fn(w_sa))
-        nu_loss += jnp.mean(w_sa * e_nu_lamb)
-        nu_loss += gradient_penalty * jnp.mean(jax.nn.relu(nu_grad_norm - 5)**2)
-        """
-        pass
+        s, a, r, c, s_next, dones, s_0 = self.memory.sample(self.batch_size)
+        n = s.shape[0]
+
+        nu_loss = (
+            (1 - gamma) * torch.mean(self.nu_network(s_0))
+            - alpha * torch.mean(self.f())
+        )
+
+        nu = self.nu_network(s)
+        nu_next = self.nu_network(s_next)
+        nu_0 = self.nu_network(s_0)
+
+        lamb = torch.clip(torch.exp(self.lamb), 0, 1e3)
+        chi = self.chi_network(s)
+
+        e_nu_lamb = (
+            r - torch.sum(c * lamb.detach(), axis=-1)
+            + self.gamma * nu_next
+            - nu
+        )
+
+        w = np.maximum(self.f_prime_inv(e_nu_lamb / self.alpha), 0)
+
+        # nu loss [Eq 23]
+        nu_loss = (
+            (1 - gamma) * torch.mean(nu_0)
+            - self.alpha * torch.mean(self.f(w))
+            + torch.mean(w * e_nu_lamb)
+        )
+
+        # chi tau loss
+        chi_0 = self.chi_network(s_0)
+        chi = self.chi_network(s)
+        chi_next = self.chi_network(s_next)
+
+        tau = torch.exp(self.tau) + 1e-6
+
+        ell = (
+            (1 - self.gamma) * chi_0
+            + w.detach()[:, None] * (
+                c + self.gamma * chi_next - chi
+            )
+        )
+        logits = ell / tau.detach()
+        weights = torch.nn.functional.softmax(logits, axis=0) * n
+        log_weights = torch.nn.functional.log_softmax(logits, axis=0) + torch.log(n)
+        kl_divergence = torch.mean(
+            weights * log_weights - weights + 1, axis=0
+        )
+        cost_ub = torch.mean(weights * w.detach()[:, None] * c, axis=0)
+        chi_tau_loss = (
+            torch.sum(torch.mean(weights * ell, axis=0))
+            + torch.sum(-tau * (kl_divergence.detach() - self.cost_ub_eps))
+        )
+
+        # lambda loss [Eq 26]
+        lamb_loss = torch.dot(
+            lamb,
+            self.c_hats - cost_ub.deatch()
+        )
+
+        loss = nu_loss + lamb_loss + chi_tau_loss
+
+
+if __name__ == '__main__':
+    a = COptiDICE(2, 2, {})
