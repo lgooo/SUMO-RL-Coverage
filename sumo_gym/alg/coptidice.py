fix coptidice loss computation

sumo_gym/alg/coptidice.py
@@ -6,6 +6,7 @@ import random
 import math
 import numpy as np
 from util import Deque
+from util import load_offline_data
 from logger import Logger
 from alg.alg_base import Alg
 
@@ -14,7 +15,10 @@ class MLP(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dims=[128, 128], final_activation=None):
         super(MLP, self).__init__()
 
-        self.final_activation = final_activation
+        if final_activation == 'softmax':
+            self.final_activation = nn.Softmax(dim=1)
+        else:
+            self.final_activation = None
         dimensions = [input_dim] + hidden_dims + [output_dim]
         self.layers = [
             nn.Linear(dimensions[i], dimensions[i + 1])
@@ -25,44 +29,46 @@ class MLP(nn.Module):
         x = torch.flatten(x, start_dim=1)
         for layer in self.layers[:-1]:
             x = F.relu(layer(x))
-        if self.final_activation == 'softmax':
-            return nn.Softmax(self.layers[-1](x))
+        if self.final_activation is not None:
+            return self.final_activation(self.layers[-1](x))
         return self.layers[-1](x)
 
 
 class COptiDICE(Alg):
     def __init__(self, n_states, n_actions, config):
+        super(COptiDICE, self).__init__()
+
         self.config = config
         self.n_actions = n_actions
 
         self.gamma = config.get('gamma', 0.95)
         self.alpha = config.get('alpha', 1)
-        self.memory_size = config.get('memory_size', 1e5)
+        self.batch_size = config.get('batch_size', 7)
         self.f_type = config.get('f_type', 'kl')
-        self.num_costs = config.get('num_costs', 3) # TODO: this is hacky
+        self.num_costs = config.get('num_costs', 2) # TODO: this is hacky
         self.c_hats = torch.ones(self.num_costs) * config.get('cost_thresholds', 1)
         self.cost_ub_eps = config.get('cost_ub_epsilon', 0)
 
         self.nu_network = MLP(n_states, 1, [128, 128])
-        self.chi_network = MLP(n_states, 1, [128, 128])
+        self.chi_network = MLP(n_states, self.num_costs, [128, 128])
         self.policy_network = MLP(n_states, n_actions, [128, 128], 'softmax')
 
         self.lamb = torch.zeros(self.num_costs, requires_grad=True)
         self.tau = torch.zeros(self.num_costs, requires_grad=True)
 
-        self.memory = Deque(capacity=int(self.memory_size), multi_steps=1)
+        self.memory = load_offline_data(config['offline_data'], capacity=int(1e6))
 
     def f(self, x):
         if self.f_type == 'chisquare':
             return 0.5 * ((x - 1) ** 2)
         if self.f_type == 'softchi':
-            return np.where(
+            return torch.where(
                 x < 1,
-                x * (np.log(x + 1e-10) - 1) + 1,
+                x * (torch.log(x + 1e-10) - 1) + 1,
                 0.5 * ((x - 1) ** 2)
             )
         if self.f_type == 'kl':
-            x * np.log(x + 1e-10)
+            return x * torch.log(x + 1e-10)
 
         raise NotImplementedError('undefined f_type', self.f_type)
 
@@ -70,24 +76,33 @@ class COptiDICE(Alg):
         if self.f_type == 'chisquare':
             return x + 1
         if self.f_type == 'softchi':
-            return np.where(
+            return torch.where(
                 x < 0,
-                np.exp(np.minimum(x, 0)),
+                torch.exp(torch.minimum(x, 0)),
                 x + 1
             )
         if self.f_type == 'kl':
-            return np.exp(x - 1)
+            return torch.exp(x - 1)
 
         raise NotImplementedError('undefined f_type', self.f_type)
 
     def update(self):
-        s, a, r, c, s_next, dones, s_0 = self.memory.sample(self.batch_size)
-        n = s.shape[0]
+        s, a, r, c, s_next, dones = self.memory.sample(self.batch_size)
+        s_0 = s # TODO: hack since we don't log s_0 yet.
+        n = len(a)
 
-        nu_loss = (
-            (1 - gamma) * torch.mean(self.nu_network(s_0))
-            - alpha * torch.mean(self.f())
-        )
+        s = torch.tensor(np.array(s), device=self.device, dtype=torch.float)
+        a = torch.tensor(a, device=self.device, dtype=torch.int64).unsqueeze(1)
+        r = torch.tensor(r, device=self.device, dtype=torch.float).unsqueeze(1)
+
+        c = [[x['crash'], x['off_road']] for x in c]
+        c = torch.tensor(np.array(c), device=self.device, dtype=torch.float)
+        s_next = torch.tensor(np.array(s_next), device=self.device, dtype=torch.float)
+        dones = torch.tensor(dones, device=self.device, dtype=torch.float)
+        s_0 = torch.tensor(np.array(s_0), device=self.device, dtype=torch.float)
+
+        gamma = self.gamma
+        alpha = self.alpha
 
         nu = self.nu_network(s)
         nu_next = self.nu_network(s_next)
@@ -97,12 +112,12 @@ class COptiDICE(Alg):
         chi = self.chi_network(s)
 
         e_nu_lamb = (
-            r - torch.sum(c * lamb.detach(), axis=-1)
+            r - torch.sum(c * lamb.detach(), axis=-1, keepdim=True)
             + self.gamma * nu_next
             - nu
         )
 
-        w = np.maximum(self.f_prime_inv(e_nu_lamb / self.alpha), 0)
+        w = torch.relu(self.f_prime_inv(e_nu_lamb / self.alpha))
 
         # nu loss [Eq 23]
         nu_loss = (
@@ -118,19 +133,20 @@ class COptiDICE(Alg):
 
         tau = torch.exp(self.tau) + 1e-6
 
+        # [Eq 18]
         ell = (
-            (1 - self.gamma) * chi_0
-            + w.detach()[:, None] * (
-                c + self.gamma * chi_next - chi
+            (1 - gamma) * chi_0
+            + w.detach() * (
+                c + gamma * chi_next - chi
             )
         )
         logits = ell / tau.detach()
-        weights = torch.nn.functional.softmax(logits, axis=0) * n
-        log_weights = torch.nn.functional.log_softmax(logits, axis=0) + torch.log(n)
+        weights = torch.nn.functional.softmax(logits, dim=0) * n
+        log_weights = torch.nn.functional.log_softmax(logits, dim=0) + np.log(n)
         kl_divergence = torch.mean(
             weights * log_weights - weights + 1, axis=0
         )
-        cost_ub = torch.mean(weights * w.detach()[:, None] * c, axis=0)
+        cost_ub = torch.mean(weights * w.detach() * c, axis=0)
         chi_tau_loss = (
             torch.sum(torch.mean(weights * ell, axis=0))
             + torch.sum(-tau * (kl_divergence.detach() - self.cost_ub_eps))
@@ -139,18 +155,20 @@ class COptiDICE(Alg):
         # lambda loss [Eq 26]
         lamb_loss = torch.dot(
             lamb,
-            self.c_hats - cost_ub.deatch()
+            self.c_hats - cost_ub.detach()
         )
 
         loss = nu_loss + lamb_loss + chi_tau_loss
 
         # policy loss
 
-        p = self.policy_network(s).gather(1, a[:, None]).flatten()
-
+        p = self.policy_network(s).gather(1, a).flatten()
         policy_loss = -torch.mean(w.detach() * torch.log(p))
 
 
-
 if __name__ == '__main__':
-    a = COptiDICE(2, 2, {})
+    import yaml
+    with open('config/coptidice.yaml', 'r') as f:
+        conf = yaml.safe_load(f)
+    a = COptiDICE(35, 5, conf['alg'])
+    a.update()

sumo_gym/alg/ddqn.py
@@ -28,9 +28,10 @@ class MLP(nn.Module):
 
 class DDQN(Alg):
     def __init__(self, n_states, n_actions, config):
+        super(DDQN, self).__init__()
+
         self.config = config
         self.n_actions = n_actions
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.gamma = config.get('gamma',0.99)
         # epsilon-greedy
         self.frame_idx = 0