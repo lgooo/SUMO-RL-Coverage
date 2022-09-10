7764726342a043e43d6abf345b880874d3dd4fe4
Kihyuk Hong <kihyukh@umich.edu>
2022-09-10 18:24:35 -0400

    evaluator

--- sumo_gym/evaluate.py
+++ sumo_gym/evaluate.py
@@ -2,103 +2,88 @@
 # Copyright (C) 2021 University of Michigan Transportation Research Institute
 
 import argparse
-from sumo_gym import SumoGym
-import numpy as np
-import yaml
-from alg.alg_base import Alg
-from tensorboardX import SummaryWriter
-import os
-import sys
-import shutil
-import datetime
-from logger import Logger
+from itertools import groupby
+import json
 from collections import defaultdict
-
-Observation = np.ndarray
-Action = np.ndarray
+import os
 
 parser = argparse.ArgumentParser(
     description='SUMO Gym Tester')
 parser.add_argument(
-    '--config',
-    default='config/simple.yaml',
-    help='config file path')
+    '--log_dir',
+    required=True,
+    help='direction to the simulation log file')
 parser.add_argument(
-    '--num_episodes',
-    type=int,
-    default=500,
-    help='number of episodes to run'
-)
-parser.add_argument(
-    '--render',
-    action='store_true',
-    default=False,
-    help='whether to use gui rendering',
-)
-parser.add_argument(
-    '--delta_t',
-    type=float,
-    default=0.1,
-    help='simulation time step')
-parser.add_argument(
-    '--seed',
-    type=int,
-    default=None,
-    help='random seed',
-)
-parser.add_argument(
-    '--model_path',
-    help='path of the model to be tested'
-)
+    '--output',
+    help='output directory')
 args = parser.parse_args()
 
-with open(args.config, 'r') as f:
-    conf = yaml.safe_load(f)
+def parse_line(stream):
+    for line in stream:
+        (
+            episode_id, time_step, initial_state, obs, next_obs,
+            action, reward, safety, terminate, done, info,
+        ) = line.rstrip('\n').split('\t')
+        episode_id = int(episode_id)
+        time_step = int(time_step)
+        initial_state = json.loads(initial_state)
+        obs = json.loads(obs)
+        next_obs = json.loads(next_obs)
+        action = int(action)
+        reward = float(reward)
+        safety = json.loads(safety)
+        terminate = int(terminate)
+        done = int(done)
+        info = json.loads(info)
+        yield {
+            'episode_id': episode_id,
+            'time_step': time_step,
+            'initial_state': initial_state,
+            'obs': obs,
+            'next_obs': next_obs,
+            'action': action,
+            'reward': reward,
+            'safety': safety,
+            'terminate': terminate,
+            'done': done,
+            'info': info,
+        }
 
-env = SumoGym(
-    config=conf['env'],
-    delta_t=args.delta_t,
-    render_flag=args.render,
-    seed=args.seed
-)
+def evaluate_one(group):
+    ret = defaultdict(float)
+    for entry in group:
+        ret['reward'] += entry['reward'] * (0.99 ** (entry['time_step'] - 1))
+        ret['off_road'] += entry['safety']['off_road']
+        ret['near_off_road'] += entry['safety']['near_off_road']
+        ret['crash'] += entry['safety']['crash']
+        ret['near_crash'] += entry['safety']['near_crash']
+        ret['time_steps'] += 1
+    return ret
 
-agent = Alg.create(conf['alg'])
-if args.seed is not None:
-    agent.set_seed(args.seed)
+def format_one(info):
+    columns = [
+        'reward',
+        'off_road',
+        'near_off_road',
+        'crash',
+        'near_crash',
+        'time_steps',
+    ]
+    return '\t'.join([
+        str(info[x]) for x in columns
+    ])
 
-def obs_filter(obs:Observation):
-    if len(obs):
-        if obs.max()>1e3:
-            print(obs.max())
-            return False
-        elif obs.min()<-1e3:
-            print(obs.min())
-            return False
-        else:
-            return True
-    return False
+g = open(args.output, 'w')
+file_list = os.listdir(args.log_dir)
+for filename in file_list:
+    if not filename.endswith('.log'):
+        continue
+    with open(os.path.join(args.log_dir, filename), 'r') as f:
+        f.readline() # skip header
+        for episode_id, group in groupby(parse_line(f), key=lambda x: x['episode_id']):
+            ret = format_one(evaluate_one(group))
+            print('\t'.join([
+                filename, str(episode_id), ret
+            ]), file=g)
 
-def policy(obs: Observation) -> Action:
-    return agent.choose_action(obs)
-
-agent.load(args.model_path)
-# no e-greedy
-agent.epsilon_start = 0
-agent.epsilon_end = 0
-num_crashes = 0
-num_out_of_roads = 0
-for _ in range(args.num_episodes):
-    obs = env.reset()
-    terminate = False
-    episode_reward = 0
-    episode_steps = 0
-    while not terminate:
-        action = policy(obs)
-        obs, reward, terminate, done, info = env.step(action=agent.continuous_action(action))
-        if info.get('crash'):
-            num_crashes += 1
-        if info.get('out_of_road'):
-            num_out_of_roads += 1
-        episode_steps += 1
-    env.close()
-print(num_crashes / args.num_episodes)
+g.close()
